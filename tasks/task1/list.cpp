a43078d9e5da4a4a39190cfea56a35b0eda7b4a2
Карина Иванова <KYIvanova@stud.kpfu.ru>
2023-10-23 09:47:29 +0300

    Задание 1

--- tasks/task1/list.cpp
+++ tasks/task1/list.cpp
@@ -3,21 +3,31 @@
 
 List::List()
 {
-
+    first = nullptr;
+    last = nullptr;
 }
 
 
 bool
 List::is_empty()
 {
-
+  if (first == nullptr) 
+  {
+    return true;
+  } 
+  else
+  {
+    return false;
+  }
 }
 
 
 void
 List::clear()
 {
-
+    while (first != nullptr){
+        remove_back();
+    }
 }
 
 
@@ -25,13 +35,34 @@ List::clear()
 void
 List::push_front(int _val)
 {
-
+    Node* push = new Node (_val);
+    if (is_empty()== true) 
+    {
+        first = push;
+        last = push;
+    }
+    else 
+    {
+        push -> next = first;
+        first = push;
+    }
 }
 
 
 void
 List::push_back(int _val)
 {
+     Node* push = new Node (_val);
+    if (is_empty()== true) 
+    {
+        first = push;
+        last = push;
+    }
+    else
+    {
+        last -> next = push;
+        last = push;
+    }
 
 }
 
@@ -39,6 +70,18 @@ List::push_back(int _val)
 Node*
 List::find(int _val)
 {
+    if (is_empty())
+    {
+        return nullptr;
+    }
+    Node* ind = first;
+    while (ind != nullptr){
+        if (ind -> val == _val){
+            return ind;
+        }
+        ind = ind ->next;
+    }
+    return nullptr;
 
 }
 
@@ -46,6 +89,18 @@ List::find(int _val)
 void
 List::remove_front()
 {
+    if (is_empty()){
+        return;
+    }
+    if (first == last)
+    {
+        first = nullptr;
+        last = nullptr;
+        return;
+    }
+    Node* _first = first;
+    first = first -> next;
+    delete _first;
 
 }
 
@@ -53,12 +108,43 @@ List::remove_front()
 void
 List::remove_back()
 {
-
+    if (is_empty()){
+        return;
+    }
+    if (first == last)
+    {
+        first = nullptr;
+        last = nullptr;
+        return;
+    }
+    Node* prelast = first;
+    while (prelast -> next != last){
+        prelast = prelast ->next;
+    }
+    prelast -> next = nullptr;
+    last = prelast;
 }
 
 
 bool
 List::remove(const Node* _node)
 {
-
+     if (is_empty() || _node == nullptr || find(_node -> val) == nullptr){
+        return false;
+    }
+    if (_node == first){
+        remove_front();
+        return true;
+    }
+    if (_node == last){
+        remove_back();
+        return true;
+    }
+    Node* ind = first;
+    while (ind ->next != _node){
+        ind = ind ->next;
+    }
+    ind->next = _node->next;
+    delete _node;
+    return true;
 }
