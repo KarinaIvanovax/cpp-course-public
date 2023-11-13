1fe6ee19ce1ac43decd7420bdbcc0372c999368b
KarinaIvanovax <112822612+KarinaIvanovax@users.noreply.github.com>
2023-11-13 11:07:50 +0300

    Задание 3

--- tasks/task3/main.cpp
+++ tasks/task3/main.cpp
@@ -4,6 +4,18 @@
 
 int main()
 {
+    int arr[] = {1, 2, 4, 4, 6, 7};
+    int digit;
+    std::cin >> digit;
+    int* result = LowerBound(std::begin(arr), std::end(arr), digit);
+
+    if (result != std::end(arr)) {
+        std::cout << "Первое большее или равное число: " << *result << std::endl;
+    } else {
+        std::cout << "Числа больше введенного нет" << std::endl;
+    }
+
+    return 0;
     // nullptr - не должны падать, вернуть nullptr
     // ТУТ ЗАГЛУШКА, ЧТОБЫ КОД КОМПИЛИРОВАЛСЯ
     LowerBound(nullptr, nullptr, 0);
