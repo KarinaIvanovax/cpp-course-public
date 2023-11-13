baab9084f03a4d30f1458d4095c320eef13a7110
KarinaIvanovax <112822612+KarinaIvanovax@users.noreply.github.com>
2023-11-13 11:06:54 +0300

    Задание 3

---
+++ bin_search.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+int* LowerBound(int* first, int* last, int value) {
+	while (first < last) {
+        int* mid = first + (last - first) / 2;
+        if (*mid < value) {
+            first = mid + 1;
+        } else {
+            last = mid;
+        }
+    }
+    return last;
+}
